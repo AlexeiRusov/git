c1112e72341eed8159520dd528d45c937636046b
student <student@localhost>
2019-09-18 10:17:32 +0300

    Homework/ added

---
+++ Homework/script.js
@@ -0,0 +1,6 @@
+var jaketPrice = 10000;
+var byciclePrice = 999;
+var socksPrice =  3000;
+var points =  parseInt ((jaketPrice + byciclePrice + socksPrice) / 1000) * 100; 
+var resultStr = points + ' балл(ов)'
+console.log(resultStr);
