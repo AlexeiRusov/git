32031d01b7b07ba6a484f0dce7619816db847912
student <student@localhost>
2019-09-19 09:58:46 +0300

    some changes

--- Homework/script.js
+++ Homework/script.js
@@ -1,6 +1,10 @@
-var jaketPrice = 10000;
-var byciclePrice = 999;
-var socksPrice =  3000;
-var points =  parseInt ((jaketPrice + byciclePrice + socksPrice) / 1000) * 100; 
-var resultStr = points + ' балл(ов)'
+const jaketPrice = 50;
+const byciclePrice = 3000;
+const socksPrice =  1;
+const purchaseAmount = jaketPrice + byciclePrice + socksPrice;
+const coefficientPoints =  parseInt ((purchaseAmount) / 1000); 
+const points = coefficientPoints * 100;
+const resultStr = 'Вам начисленно ' + points + ' бонусов.'
+console.log ('Ваша покупка составила ' + purchaseAmount + ' рублей');
 console.log(resultStr);
+console.log ('Клубная программа Спормастер, Один бонус = один рубль, Сумма каждой покупки заносится на вашу карту.')
